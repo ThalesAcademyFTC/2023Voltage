551ec1ef14a964bbef4edc39bc6cfc99b68158a8
Gabe Walker <gjwalke2@ncsu.edu>
2023-11-03 16:33:17 -0400

    Added old files back to demo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demo/NewFeatureDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demo/NewFeatureDemo.java
@@ -1,8 +1,10 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.demo;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Spark;
 
 @TeleOp()
 public class NewFeatureDemo extends OpMode {
